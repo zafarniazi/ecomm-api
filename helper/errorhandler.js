1be30872fb17ad9939c42235e608834a62f16b3a
zafar <zafarkhan808045@gmail.com>
2022-01-29 18:47:26 +0500

    initial commist

--- helper/errorhandler.js
+++
@@ -1,6 +0,0 @@
-const errorhandler = function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).send('invalid token...');
-    }
-}
-module.exports = errorhandler;
